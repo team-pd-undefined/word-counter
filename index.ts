c377d036f7a7765b5861e995e1b2ca0ea1b56b21
yhc0527 <yhc0527@gmail.com>
2019-04-15 15:07:18 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+const { readFileSync } = Deno;
+
+function main() {
+    console.time('Counter');
+    const TXT_FILE = "./odyssey.mb-2.txt";
+    const decoder = new TextDecoder('utf-8');
+    const content = decoder.decode(readFileSync(TXT_FILE));
+    const words = content.match(/\w+/gi)
+
+    const counterMap = new Map<string, number>();
+    
+    words.forEach(word => {
+        if(counterMap.has(word)) {
+            counterMap.set(word, counterMap.get(word) + 1);
+        } else {
+            counterMap.set(word, 0);
+        }
+    });
+
+    counterMap.forEach((value, key) => {
+        console.log(`${key} : ${value}`);
+    });
+
+    console.timeEnd('Counter')
+}
+
+main();
